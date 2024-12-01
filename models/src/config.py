3acca1a91d1a89235ac6dbf7310b9f8ff256fa23
UICHEOL-HWANG <icuchoel@gmail.com>
2024-12-01 16:07:50 +0900

    Chore: 자잘한 부분 개선

--- models/src/config.py
+++ models/src/config.py
@@ -1,12 +1,12 @@
 class Config:
     ID2LABEL = {
-        0: '분노',    # angry
-        1: '행복',    # happy
-        2: '슬픔',    # sad
-        3: '공포',    # fear
-        4: '혐오',    # disgust
-        5: '놀람',    # surprise
-        6: '중립'     # neutral
+        '공포': 0,
+        '놀람': 1,
+        '분노': 2,
+        '슬픔': 3,
+        '중립': 4,
+        '행복': 5,
+        '혐오': 6
     }
 
     LABEL2ID = {label: idx for idx, label in ID2LABEL.items()}
